c3fc06c7ea216ada1a7d45674a62908cb69671ee
cperez26 <42821682+cperez26@users.noreply.github.com>
2019-05-21 18:37:18 -0500

    segundo commit (se agregaron celdas)

--- agenda.js
+++ agenda.js
@@ -15,4 +15,20 @@ export default class Tabla {
             this._tabla.deleteRow(i);
         }
     }
+
+    _addToTable(objContactos){
+        let row = this._tabla.insertRow(-1);
+        let cell = row.insertCell(0);
+        cell.innerHTML = objContactos.nombre;
+        cell = row.insertCell(1);
+        cell.innerHTML = objContactos.telefono;
+        cell = row.insertCell(2);
+        cell.innerHTML = objContactos.direccion;
+        cell = row.insertCell(3);
+        cell.innerHTML = objContactos.email;
+        cell = row.insertCell(4);
+        cell.innerHTML = objContactos.birthday;
+        cell = row.insertCell(5);
+        cell.innerHTML = objContactos.age;
+    }
 }
